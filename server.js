d1e58c9757ed8aef0644853fb3cfcf88e02a9b60
Andrew <manuele.andrew+Git@gmail.com>
2017-04-26 15:27:17 -0400

    working version of 3 HTML pages.

--- server.js
+++ server.js
@@ -6,19 +6,36 @@ var fs = require("fs");
 const PORT = 80;
 
 // App index
-
 app.get('/index.html', function (req, res) {
   res.sendFile(__dirname + "/" + "index.html");
 });
 
 app.get('/fullUsers.html', function (req, res) {
-  res.sendFile(__dirname + "/" + "fullUsers.html");
-});
+   fs.readFile( __dirname + "/" + "sleepers.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
 
 app.get('/specificUser.html', function (req, res) {
   res.sendFile(__dirname + "/" + "specificUser.html");
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.get('/specificUser.html/:id', function (req, res) {
+	// First read existing users.
+	fs.readFile( __dirname + "/" + "sleepers.json", 'utf8', function (err, data) {
+       users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+	});
+})
+
+var server = app.listen(PORT, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
 
